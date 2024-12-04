42bc3b493a8a89884e44757523ce55e896e8d333
haradeva <haradev555@gmail.com>
2024-12-04 07:17:28 +0530

    Add useCart functionality to ProductCard.js

--- react-module_end-e_commerce-gh/src/components/ProductCard.js
+++ react-module_end-e_commerce-gh/src/components/ProductCard.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { useCart } from '../context/cartContext';
+
+
 const ProductCard = ({ product }) => {
   const stylesContainer = {
     border: "1px solid #ccc",
@@ -9,12 +13,15 @@ const ProductCard = ({ product }) => {
     height: "150px",
   };
 
+const { addToCart } = useCart();
+
   return (
     <div style={stylesContainer}>
       <img src={product.image} style={stylesImage} />
       <h2>{product.name}</h2>
       <p>{product.description}</p>
       <p>${product.price.toFixed(2)}</p>
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
     </div>
   );
 };
