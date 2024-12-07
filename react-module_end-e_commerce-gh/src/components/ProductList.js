2ca18ed9280e81ed0650116bdcf36f93f1ce3b93
haradeva <haradev555@gmail.com>
2024-12-07 09:10:07 +0530

    Update ProductList

--- react-module_end-e_commerce-gh/src/components/ProductList.js
+++ react-module_end-e_commerce-gh/src/components/ProductList.js
@@ -1,5 +1,7 @@
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
+import Filter from "./Filter";
 import ProductCard from "./ProductCard";
-import products from "../products.json";
 
 const stylesList = {
   display: "flex",
@@ -7,12 +9,35 @@ const stylesList = {
   justifyContent: "space-around",
 };
 
-const ProductList = () => {
+const ProductList = ({ products }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleSearchChange = (term) => {
+    setSearchTerm(term);
+  };
+  const handleFilterChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredProducts = products.filter((product) => {
+    const matchesSearch = product.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const matchesCategory = selectedCategory
+      ? product.category === selectedCategory
+      : true;
+    return matchesSearch && matchesCategory;
+  });
   return (
-    <div style={stylesList}>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div>
+      <SearchBar onSearchChange={handleSearchChange} />
+      <Filter onFilterChange={handleFilterChange} />
+      <div style={stylesList}>
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
